b220cb60296b2e1e672403cc127cba98741ab7ae
karnsens <boonlapotk@gmail.com>
2023-03-23 13:49:00 +0700

    del comment

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// const {getSecretKey,databaseName} = require('./settings');
-// console.log(getSecretKey());
-// console.log(databaseName);
-
 const myObject = require('./settings');
 console.log(myObject.getSecretKey());
 console.log(myObject.databaseName);
